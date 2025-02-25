7fa254b1a32ea4f66293d96db3bc40c5f26d2b4c
Vaibhavi Jha <vaibhavi0154.becse24@chitkara.edu.in>
2025-02-25 20:45:12 +0530

    changes

---
+++ chat-app/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("message", (data) => {
+        io.emit("message", data); // Send message to all users
+    });
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected:", socket.id);
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
